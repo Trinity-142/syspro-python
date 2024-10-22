f0d55d733a9594d85ca617ad6bc3a3cc23c690bf
Nikita Sharapov <n.sharapov.nsk@gmail.com>
2024-10-22 19:15:16 +0700

    mini #5

---
+++ minis/tests/mini_5_tests.py
@@ -0,0 +1,36 @@
+from solutions.mini_5 import specialize, sum
+
+
+def test():
+    y_const = specialize(sum, y=1)
+    assert y_const(10) == 11
+
+    const_y_sep = specialize(sum, 1)
+    assert const_y_sep(y=10) == 11
+
+    const_y = specialize(sum, 1, y=10)
+    assert const_y() == 11
+
+    x_const = specialize(sum, x=1)
+    assert x_const(10) == 11
+
+    const_x_sep = specialize(sum, 1)
+    assert const_x_sep(x=10) == 11
+
+    const_x = specialize(sum, 1, x=10)
+    assert const_x() == 11
+
+    x_y = specialize(sum, x=1, y=5)
+    assert x_y() == 6
+
+    x_y_sep = specialize(sum, x=1)
+    assert x_y_sep(y=5) == 6
+
+    y_x = specialize(sum, y=1, x=5)
+    assert y_x() == 6
+
+    y_x_sep = specialize(sum, y=1)
+    assert y_x_sep(x=5) == 6
+
+
+test()
