7949199496212eb62ef2c65c93b320ff422b6a1a
Trinity-142 <n.sharapov.nsk@gmail.com>
2024-12-22 22:45:57 +0700

    changes in #10

--- minis/solutions/mini_10.py
+++ minis/solutions/mini_10.py
@@ -1,27 +1,29 @@
 import functools
 
-
-class Singleton:
-    instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if cls.instance is None:
-            cls.instance = super().__new__(cls, *args, **kwargs)
-        return cls.instance
-
-
 def singleton(cls):
     orig_new = cls.__new__
+    orig_init = cls.__init__
     instance = None
+    initialized = False
 
     @functools.wraps(orig_new)
     def __new__(cls, *args, **kwargs):
         nonlocal instance
+        nonlocal initialized
         if instance is None:
             instance = orig_new(cls, *args, **kwargs)
+            orig_init(instance, *args, **kwargs)
+            initialized = True
         return instance
-
     cls.__new__ = __new__
+
+    def __init__(self, *args, **kwargs):
+        nonlocal initialized
+        if not initialized:
+            orig_init(self, *args, **kwargs)
+            initialized = True
+    cls.__init__ = __init__
+
     return cls
 
 
@@ -30,19 +32,7 @@ class DecoratedCounter:
     pass
 
 
-class Counter:
-    pass
-
-
-class MixinCounter(Singleton, Counter):
-    pass
-
-
 if __name__ == '__main__':
-    mc1 = MixinCounter()
-    mc2 = MixinCounter()
-    assert id(mc1) == id(mc2)
-
     dc1 = DecoratedCounter()
     dc2 = DecoratedCounter()
     assert id(dc1) == id(dc2)
